11bf298240844b9044e50719e754f9b03d8f2ae1
suleymanozay <suleymanozay@gmail.com>
2020-01-18 13:47:08 -0500

    aven pro

---
+++ src/test/java/com/pratice/Testing.java
@@ -0,0 +1,14 @@
+package com.pratice;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.hrms.utils.CommonMethods;
+
+public class Testing extends CommonMethods {
+		@Test
+		public void testing() {
+			driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		}
+
+}
